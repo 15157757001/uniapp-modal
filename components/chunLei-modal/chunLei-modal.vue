7115e4779eaea8321dc5e2fbefe875a64d7d9d06
mxphpxm <a949600520@qq.com>
2019-09-27 18:10:50 +0800

    u:'1.0.0'

---
+++ components/chunLei-modal/chunLei-modal.vue
@@ -0,0 +1,373 @@
+<template>
+	<view class="mask" @tap="tapMask" :class="!value?'':'mask-show'" @touchmove.stop.prevent>
+		<block v-if="type=='default'">
+			<view class="default-view" @tap.stop>
+				<view class="title">{{mData.title}}</view>
+				<view class="content word-break">{{mData.content}}</view>
+				<view class="btn">
+					<view class="cancel" @tap="tapCancel" :style="{color:mData.cancelColor?mData.cancelColor:''}">{{mData.cancelText?mData.cancelText:'取消'}}</view>
+					<view class="confirm" @tap="tapConfirm" :style="{color:mData.confirmColor?mData.confirmColor:''}">{{mData.confirmText?mData.confirmText:'确认'}}</view>
+				</view>
+			</view>
+		</block>
+		<block v-if="type=='select'">
+			<view class="select-view" @tap.stop>
+				<view v-for="(item,index) in mData" :key="index" class="select-box" @tap="tapConfirm(item)">
+					<view><image :src="item.icon" v-if="item.icon"></image>{{item.title}}</view>
+					<view class="content">{{item.content}}</view>
+				</view>
+			</view>
+		</block>
+		<block v-if="type=='advert'">
+			<view class="advert-view">
+				<image :src="mData.src" class="confirm" @tap.stop="tapConfirm" :style="{width:mData.width?mData.width:'500rpx',height:mData.height?mData.height:'700rpx'}"></image>
+				<image class="cancel" @tap.stop="tapCancel" src="../../static/chunLei-modal/close.png"></image>
+			</view>
+		</block>
+		<block v-if="type=='share'">
+			<view class="share-view" @tap.stop>
+				<view v-for="(item,index) in mData" :key="index" class="share-box" @tap="tapConfirm(item)">
+					<image :src="item.icon" v-if="item.icon"></image>{{item.title}}
+				</view>
+			</view>
+		</block>
+		<block v-if="type=='input'">
+			<view class="input-view" @tap.stop>
+				<view class="title">{{mData.title}}</view>
+				<view class="content">
+					<view v-for="(item,index) in mData.content" :key="index" class="input-box">
+						<view>{{item.title}}</view>
+						<block v-if="item.type=='number'">
+							<input v-model="item.content" type="number" :password="item.type=='password'" :placeholder="item.placeholder"/>
+						</block>
+						<block v-if="item.type=='password'">
+							<input v-model="item.content" :password="item.type=='password'" :placeholder="item.placeholder"/>
+						</block>
+						<block v-if="item.type=='digit'">
+							<input v-model="item.content" type="digit" :password="item.type=='password'" :placeholder="item.placeholder"/>
+						</block>
+						<block v-if="item.type=='idcard'">
+							<input v-model="item.content" type="idcard" :password="item.type=='password'" :placeholder="item.placeholder"/>
+						</block>
+						<block v-if="!item.type||item.type=='text'">
+							<input v-model="item.content" type="text" :password="item.type=='password'" :placeholder="item.placeholder"/>
+						</block>
+					</view>
+				</view>
+				<view class="btn">
+					<view class="cancel" @tap="tapCancel" :style="{color:mData.cancelColor?mData.cancelColor:''}">{{mData.cancelText?mData.cancelText:'取消'}}</view>
+					<view class="confirm" @tap="inputConfirm" :style="{color:mData.confirmColor?mData.confirmColor:''}">{{mData.confirmText?mData.confirmText:'确认'}}</view>
+				</view>
+				
+			</view>
+		</block>
+		<block v-if="type=='custom'">
+			<slot></slot>
+		</block>
+	</view>
+</template>
+
+<script>
+	
+	export default{
+		props:{
+			type:{
+				type:String,
+				default:'default'
+			},
+			value:{
+				type:Boolean,
+				default:false
+			},
+			maskEnable:{
+				type:Boolean,
+				default:true
+			},
+			mData:{
+				type:[Object, Array],
+				default:()=>{}
+			},
+			nav:{
+				type:Boolean,
+				default:true
+			},
+			navMask:{
+				type:Boolean,
+				default:false
+			}
+		},
+		data(){
+			return{
+				navView:null,
+				tabbarView:null
+			}
+		},
+		mounted() {
+			//#ifdef APP-PLUS
+			this.creatPlusMask()
+			//#endif
+		},
+		methods:{
+			inputConfirm(){
+				this.$emit('onConfirm',this.mData.content)
+				this.$emit('input',false)
+			},
+			tapCancel(){
+				this.$emit('onCancel')
+				this.$emit('input',false)
+			},
+			tapConfirm(item){
+				this.$emit('onConfirm',item)
+				this.$emit('input',false)
+			},
+			tapMask(){
+				if(!this.maskEnable) return
+				this.$emit('input',false)
+			},
+			async creatPlusMask(){
+				let promise = new Promise((resolve,reject)=>{
+					uni.getSystemInfo({
+						success: function(e) {
+							let customBar
+							customBar = e.statusBarHeight + 44;
+							resolve(customBar)
+						}
+					})
+				})
+				let navHeight = await promise
+				navHeight = this.nav?navHeight:0
+				this.navView = new plus.nativeObj.View('nav',{top:'0px',left:'0px',height:`${navHeight}px`,width:'100%'});
+				this.navView.draw([
+					{tag:'rect',id:'rect',color:'rgba(0,0,0,0.6)',position:{top:'0px',left:'0px',width:'100%',height:'100%'}},
+				]);
+				this.navView.addEventListener("click", (e) => {
+					this.tapMask()
+				}, false);
+				let tabbarHeight = uni.getSystemInfoSync().screenHeight - uni.getSystemInfoSync().windowHeight - navHeight
+				this.tabbarView = new plus.nativeObj.View('tabbar',{bottom:'0px',left:'0px',height:`${tabbarHeight}px`,width:'100%'});
+				this.tabbarView.draw([
+					{tag:'rect',id:'rect',color:'rgba(0,0,0,0.6)',position:{top:'0px',left:'0px',width:'100%',height:'100%'}},
+				]);
+				this.tabbarView.addEventListener("click", (e) => {
+					this.tapMask()
+				}, false);
+			},
+			animateView(){
+				// this.navView.animate({type:'shrink',frames:20,region:{top:'44px',bottom:'48px'}});
+				// this.tabbarView.animate({type:'shrink',frames:20,region:{top:'44px',bottom:'48px'}});
+			}
+		},
+		watch:{
+			value:{
+				immediate:true,
+				handler(newVal,oldVal){
+					if(!this.navMask) return
+					//#ifdef APP-PLUS
+					if(newVal) {
+						this.navView.show();
+						this.tabbarView.show();
+						
+					}else{
+						this.navView.hide();
+						this.tabbarView.hide();
+					}
+					//#endif
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.mask{
+		position: fixed;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: 9999;
+		background:rgba(0 ,0,0,0);
+		transition: background 0.3s ease-in-out;
+		display: flex;
+		align-items: center;
+		opacity: 0;
+		visibility: hidden;
+		justify-content: center;
+		&.mask-show{
+			background:rgba(0,0,0,0.6);
+			visibility:visible;
+			opacity: 1;
+		}
+		
+	}
+	.hover{
+		background: #f2f2f2;
+	}
+	.default-view{
+		width: 600rpx;
+		font-weight: 400;
+		font-size: 18px;
+		background-color: #fff;
+		border-radius: 6rpx;
+		.title{
+			height: 100rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		.content{
+			padding: 40rpx 48rpx;
+			min-height: 40px;
+			font-size: 15px;
+			line-height: 1.4;
+			color: #999;
+			text-align: center;
+		}
+		.btn{
+			height: 100rpx;
+			display: flex;
+			flex-direction: row;
+			border-top:1px solid #ccc;
+			.cancel{
+				display: flex;
+				flex: 1;
+				justify-content: center;
+				align-items: center;
+				border-right:1px solid #ccc;
+			}
+			.confirm{
+				display: flex;
+				flex: 1;
+				justify-content: center;
+				align-items: center;
+				color: rgb(0, 122, 255);
+			}
+		}
+	}
+	.select-view{
+		width: 600rpx;
+		background-color: #fff;
+		border-radius: 6rpx;
+		.select-box{
+			height: 100rpx;
+			padding: 20rpx;
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+			border-bottom: 0.5px solid #ddd;
+			.content{
+				color: #aaa;
+				font-size: 12px;
+			}
+		}
+		image{
+			display: inline-block;
+			vertical-align: middle;
+			width: 40rpx;
+			height: 40rpx;
+			margin-right: 20rpx;
+		}
+	}
+	.select-view .select-box:last-child{
+		border: none;
+	}
+	.advert-view{
+		overflow: hidden;
+		
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		.confirm{
+			width: 500rpx;
+			height: 700rpx;
+			border-radius: 6rpx;
+		}
+		.cancel{
+			margin-top: 150rpx;
+			width: 60rpx;
+			height: 60rpx;
+		}
+	}
+	.share-view{
+		width: 600rpx;
+		background-color: #fff;
+		border-radius: 6rpx;
+		padding: 20rpx;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		font-size: 18px;
+		.share-box{
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			width: 33.33%;
+			padding: 40rpx 0;
+		}
+		image{
+			width: 80rpx;
+			height: 80rpx;
+			margin-bottom: 20rpx;
+		}
+	}
+	.input-view{
+		width: 600rpx;
+		font-weight: 400;
+		font-size: 18px;
+		background-color: #fff;
+		border-radius: 6rpx;
+		.title{
+			height: 100rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border-bottom: 1px solid #ccc;
+		}
+		.content{
+			padding: 40rpx 48rpx;
+			min-height: 40px;
+			font-size: 18px;
+			
+			text-align: left;
+		}
+		.input-box{
+			display: flex;
+			margin-bottom: 20rpx;
+			view{
+				margin-right: 20rpx;
+				min-width: 150rpx;
+			}
+			input{
+				
+				font-size: 18px;
+			}
+		}
+		.btn{
+			height: 100rpx;
+			display: flex;
+			flex-direction: row;
+			border-top:1px solid #ccc;
+			.cancel{
+				display: flex;
+				flex: 1;
+				justify-content: center;
+				align-items: center;
+				border-right:1px solid #ccc;
+			}
+			.confirm{
+				display: flex;
+				flex: 1;
+				justify-content: center;
+				align-items: center;
+				color: rgb(0, 122, 255);
+			}
+		}
+	}
+	.word-break{
+		word-wrap: break-word;
+		word-break: break-all;
+		white-space: pre-wrap;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,148 @@
+<template>
+	<view class="content">
+		<button @tap="tapBtn('default')">默认</button>
+		<button @tap="tapBtn('select')">选择</button>
+		<button @tap="tapBtn('advert')">广告</button>
+		<button @tap="tapBtn('share')">分享</button>
+		<button @tap="tapBtn('input')">输入框</button>
+		<button @tap="tapBtn('custom')">自定义</button>
+		<chunLei-modal v-model="value" :mData="data" :type="type" @onConfirm="onConfirm" @onCancel="onCancel" navMask>
+			<div class="custom-view" @tap.stop>
+				<div class="hongbao">
+					<div class="top">该红包已超过24小时。如已领取,可在“红包记录”中查看</div>
+					<div class="bottom">查看领取详情></div>
+				</div>
+				<image class="cancel" @tap.stop="value=false" src="../../static/close.png"></image>
+			</div>
+		</chunLei-modal>
+	</view>
+</template>
+
+<script>
+	import chunLeiModal from '@/components/chunLei-modal/chunLei-modal.vue'
+	export default {
+		components:{
+			chunLeiModal
+		},
+		data() {
+			return {
+				value:false,
+				type:'default',
+				data:{},
+				defaultData:{title:'提示',content:'这是一个模态弹窗',cancelText:'cancel',confirmColor:'#3CC51F'},
+				selectData:[{title:'拍摄',content:'照片或视频',icon:'../../static/shoot.png'},{title:'从照片选择'}],
+				advertData:{src:'../../static/advert.jpg',width:'600rpx',height:'350rpx'},
+				shareData:[
+					{title:'朋友圈',icon:'../../static/pengyouquan.png'},
+					{title:'微信好友',icon:'../../static/weixinhaoyou.png'},
+					{title:'微博',icon:'../../static/weibo.png'},
+					{title:'QQ好友',icon:'../../static/QQhaoyou.png'},
+					{title:'QQ空间',icon:'../../static/QQkongjian.png'},
+				],
+				inputData:{
+					title:'登录',
+					content:[
+						{title:'手机号',content:'',type:'number',placeholder:'请输入手机号'},
+						{title:'密码',content:'',type:'password',placeholder:'请输入密码'},
+					]
+				}
+			
+			}
+		},
+		onLoad() {
+			
+		},
+		async mounted() {
+			
+		},
+		methods: {
+			onConfirm(e){
+				switch(this.type){
+					case 'default':
+						uni.showToast({title:'确认',icon:'none'})
+						break;
+					case 'select':
+						uni.showToast({title:e.title,icon:'none'})
+						break;
+					case 'advert':
+						uni.showToast({title:'广告',icon:'none'})
+						break;
+					case 'share':
+						uni.showToast({title:e.title,icon:'none'})
+						break;
+					case 'input':
+						console.log(e)
+						break;
+				}
+			},
+			onCancel(){
+				uni.showToast({title:'取消',icon:'none'})
+			},
+			tapBtn(type){
+				this.type = type
+				this.value = !this.value
+				switch(this.type){
+					case 'default':
+						this.data = this.defaultData
+						break;
+					case 'select':
+						this.data = this.selectData
+						break;
+					case 'advert':
+						this.data = this.advertData
+						break;
+					case 'share':
+						this.data = this.shareData
+						break;
+					case 'input':
+						this.data = this.inputData
+						break;
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content{
+		padding: 0 100rpx;
+		
+	}
+	button{
+		margin-top: 50rpx;
+	}
+	.custom-view{
+		overflow: hidden;
+		
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		.hongbao{
+			width: 500rpx;
+			height: 700rpx;
+			border-radius: 6rpx;
+			background: #F35543;
+			.top{
+				text-align: center;
+				padding: 100rpx 80rpx 0 ;
+				height: 400rpx;
+				background: #F45E4D;
+				color: #FEDCAC;
+				font-szie:30px;
+				border-bottom-right-radius: 60% 100rpx;
+				border-bottom-left-radius: 60% 100rpx;
+				box-shadow: 0 1px 4px #717171;
+			}
+			.bottom{
+				margin-top: 80rpx;
+				color: #FDC69B;
+				text-align: center;
+			}
+		}
+		.cancel{
+			margin-top: 100rpx;
+			width: 60rpx;
+			height: 60rpx;
+		}
+	}
+</style>
